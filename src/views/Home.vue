5a452609890dd4e971201e0666315ea5a5f3b472
Nate <natelalvarado@gmail.com>
2021-11-02 22:09:21 -0400

    Adds filter functionality on Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,25 @@
 <template>
   <div class="home">
+    <h3>Filter by category</h3>
+    <button v-on:click="setFilter('')">All</button>
+    <button v-on:click="setFilter('Movie')">Movie</button>
+    <button v-on:click="setFilter('Television')">Television</button>
+    <button v-on:click="setFilter('Book')">Book</button>
+    <button v-on:click="setFilter('Video Game')">Video Game</button>
+    <button v-on:click="setFilter('Music')">Music</button> <br />
+    <h3>Filter by Status</h3>
+    <button v-on:click="setFilter('Complete')">Complete</button>
+    <button v-on:click="setFilter('Incomplete')">Incomplete</button>
+    <button v-on:click="setFilter('Not Started')">Not Started</button>
     <h1>Media List</h1>
-    <div v-for="media in media" v-bind:key="media.id">
-      <p>{{ media.title }}</p>
-      <p>{{ media.category }}</p>
+    <div
+      v-for="medium in filterBy(media, filter, 'category', 'status')"
+      v-bind:key="medium.id"
+    >
+      <br />
+      <img :src="medium.image" alt="" width="250" height="250" />
+      <p>{{ medium.title }}</p>
+      <p>{{ medium.category }}</p>
       <router-link :to="`/media/${media.id}`">SHOW INFO</router-link>
     </div>
   </div>
@@ -13,10 +29,15 @@
 
 <script>
 import axios from "axios";
+import Vue2Filters from "vue2-filters";
+
 export default {
+  mixins: [Vue2Filters.mixin],
+
   data: function () {
     return {
       media: [],
+      filter: "",
     };
   },
   created: function () {
@@ -28,6 +49,17 @@ export default {
         console.log("media index", response);
         this.media = response.data;
       });
+    },
+    setSortAttribute: function (attribute) {
+      if (this.sortAttribute === attribute) {
+        this.sortOrder = this.sortOrder * -1;
+      } else {
+        this.sortAttribute = attribute;
+        this.sortOrder = 1;
+      }
+    },
+    setFilter: function (filter) {
+      this.filter = filter;
     },
   },
 };
