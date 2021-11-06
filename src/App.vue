2f71df9bc6ca073c555d194d7cb0dd65255f5df0
Nate <natelalvarado@gmail.com>
2021-11-05 21:40:59 -0500

    Adds finalized nav-bar and new login page

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,136 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <span v-if="isLoggedIn()">
-        <router-link to="/new">New Media</router-link> |
-        <router-link to="/users/me">My Account</router-link> |
-        <router-link to="/logout">Logout</router-link>
-      </span>
-      <span v-if="!isLoggedIn()">
-        <router-link to="/login">Login</router-link> |
-        <router-link to="/signup">Signup</router-link>
-      </span>
+    <!-- start main-wrapper section -->
+    <div class="main-wrapper">
+      <!-- start header section -->
+      <header class="scrollHeader">
+        <div class="navbar-default">
+          <!-- start top search -->
+          <!-- end top search -->
+
+          <div class="container">
+            <div class="row align-items-center">
+              <div class="col-md-12">
+                <div class="menu_area">
+                  <nav class="navbar navbar-expand-lg navbar-light no-padding">
+                    <div class="navbar-header navbar-header-custom">
+                      <!-- start logo -->
+                      <a
+                        href="/"
+                        class="
+                          navbar-brand
+                          width-200px
+                          sm-width-180px
+                          xs-width-150px
+                        "
+                        ><img
+                          id="logo"
+                          src="img/logos/mediapedia logo.png"
+                          alt="logo"
+                          height="200"
+                          width="100"
+                      /></a>
+                      <!-- end logo -->
+                    </div>
+
+                    <div class="navbar-toggler"></div>
+
+                    <!-- start menu area -->
+                    <ul
+                      class="navbar-nav ml-auto"
+                      id="nav"
+                      style="display: none"
+                    >
+                      <li>
+                        <a v-if="isLoggedIn()">Account Settings</a>
+                        <ul>
+                          <li><a href="/logout">Logout</a></li>
+                          <li><a href="/users/me">My Profile</a></li>
+                        </ul>
+                      </li>
+                      <li v-if="!isLoggedIn()">
+                        <a href="/login">Login</a>
+                      </li>
+                      <li v-if="!isLoggedIn()"><a href="/signup">Signup</a></li>
+                      <!-- <li>
+                        <a href="javascript:void(0)">User Panel</a>
+                        <ul>
+                          <li><a href="admin/index.html">Dashboard</a></li>
+                          <li><a href="admin/message.html">Message</a></li>
+                          <li><a href="admin/booking.html">Booking</a></li>
+                          <li><a href="admin/wallet.html">Wallet</a></li>
+                          <li>
+                            <a href="admin/my-listing.html">My Listing</a>
+                          </li>
+                          <li><a href="admin/reviews.html">Reviews</a></li>
+                          <li><a href="admin/bookmark.html">Bookmarks</a></li>
+                          <li>
+                            <a href="admin/add-listing.html">Add Listing</a>
+                          </li>
+                          <li>
+                            <a href="admin/my-profile.html">My Profile</a>
+                          </li>
+                        </ul>
+                      </li> -->
+                    </ul>
+                    <!-- end menu area -->
+
+                    <!-- start attribute navigation -->
+                    <div
+                      class="
+                        attr-nav
+                        sm-no-margin sm-margin-65px-right
+                        xs-margin-55px-right
+                      "
+                    >
+                      <ul class="top-nav-area">
+                        <li class="dropdown sm-no-margin-right">
+                          <a
+                            href="/new"
+                            class="butn listi md-padding-15px-lr sm-no-padding"
+                          >
+                            <i
+                              class="
+                                fas
+                                fa-plus-circle
+                                font-size22
+                                margin-5px-right
+                                sm-no-margin-right
+                                vertical-align-middle
+                              "
+                            ></i>
+                            <span class="vertical-align-middle sm-display-none"
+                              >ADD MEDIA</span
+                            >
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                    <!-- end attribute navigation -->
+                  </nav>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <!-- end header section -->
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <span v-if="isLoggedIn()">
+          <router-link to="/new">New Media</router-link> |
+          <router-link to="/users/me">My Account</router-link> |
+          <router-link to="/logout">Logout</router-link>
+        </span>
+        <span v-if="!isLoggedIn()">
+          <router-link to="/login">Login</router-link> |
+          <router-link to="/signup">Signup</router-link>
+        </span>
+      </div>
+      <div class="main-wrapper">
+        <router-view />
+      </div>
     </div>
-    <router-view />
   </div>
 </template>
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,107 @@
 <template>
-  <div class="login">
-    <form v-on:submit.prevent="submit()">
-      <h1>Login</h1>
-      <ul>
-        <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
-      </ul>
-      <div>
-        <label>Email:</label>
-        <input type="email" v-model="newSessionParams.email" />
+  <div class="main-wrapper">
+    <!-- start login table -->
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 center-col">
+            <div
+              class="
+                padding-40px-all
+                sm-padding-25px-all
+                shadow
+                border-radius-4
+              "
+            >
+              <h3 class="text-center margin-40px-bottom">Login</h3>
+              <form v-on:submit.prevent="submit()">
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <input
+                        class="form-control"
+                        name="username"
+                        id="username"
+                        required="required"
+                        placeholder="User Name"
+                        maxlength="70"
+                        type="email"
+                        v-model="newSessionParams.email"
+                      />
+                    </div>
+                  </div>
+                  <div class="col-12">
+                    <div class="form-group">
+                      <input
+                        class="form-control"
+                        type="password"
+                        name="password"
+                        id="password"
+                        required="required"
+                        placeholder="Password"
+                        maxlength="70"
+                        v-model="newSessionParams.password"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-sm-6 mb-2">
+                    <div class="custom-control custom-checkbox text-left">
+                      <input
+                        type="checkbox"
+                        class="custom-control-input"
+                        id="login-remember"
+                      />
+                      <label class="custom-control-label" for="login-remember"
+                        >Keep me signed in</label
+                      >
+                    </div>
+                  </div>
+                  <div class="col-sm-6 text-left text-sm-right">
+                    <a href="#!">Forgot password?</a>
+                  </div>
+                </div>
+
+                <div class="col-md-12 margin-25px-top">
+                  <button
+                    type="button"
+                    class="butn btn-block"
+                    v-on:click.prevent="submit()"
+                  >
+                    Login
+                  </button>
+                </div>
+
+                <div class="col-md-12 text-center margin-25px-top">
+                  <span
+                    >Don't have an account yet?
+                    <a href="/signup">Register</a></span
+                  >
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
-      <div>
-        <label>Password:</label>
-        <input type="password" v-model="newSessionParams.password" />
-      </div>
-      <input type="submit" value="Submit" />
-    </form>
+    </section>
+    <div class="login">
+      <form v-on:submit.prevent="submit()">
+        <h1>Login</h1>
+        <ul>
+          <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+        </ul>
+        <div>
+          <label>Email:</label>
+          <input type="email" v-model="newSessionParams.email" />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" v-model="newSessionParams.password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
   </div>
 </template>
 
